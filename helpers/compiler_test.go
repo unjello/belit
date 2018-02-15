23feb984da19e16457469060d2558a1bae6b0d66
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-02-15 21:55:50 +0100

    Add C testcases

--- helpers/compiler_test.go
+++ helpers/compiler_test.go
@@ -11,22 +11,27 @@ func init() {
 	log.SetLevel(log.PanicLevel)
 }
 
-var cppFiles = []string{
-	"file.cpp",
-	"test.cxx",
-	"package/test.cc",
-	"/outer/inner/file.c++",
-	"/test.pcc",
+var compilerFiles = []struct {
+	file     string
+	expected string
+}{
+	{"file.cpp", "cxx"},
+	{"test.cxx", "cxx"},
+	{"package/test.cc", "cxx"},
+	{"/outer/inner/file.c++", "cxx"},
+	{"/test.pcc", "cxx"},
+	{"file.c", "cc"},
+	{"/folder/file.c", "cc"},
 }
 
-func TestGetCompilerOptionsCpp(t *testing.T) {
-	for _, v := range cppFiles {
-		actual, err := GetCompilerOptions(v)
+func TestGetCompilerOptions(t *testing.T) {
+	for _, v := range compilerFiles {
+		actual, err := GetCompilerOptions(v.file)
 		if err != nil {
 			t.Errorf("GetCompilerOptions(%s): expected no error, actual: %s", v, err)
 		}
-		if actual.CompilerEnv != "cxx" {
-			t.Errorf("GetCompilerOptions(%s): expected %s, actual %s", v, "cxx", actual.CompilerEnv)
+		if actual.CompilerEnv != v.expected {
+			t.Errorf("GetCompilerOptions(%s): expected %s, actual %s", v.file, v.expected, actual.CompilerEnv)
 		}
 	}
 }
