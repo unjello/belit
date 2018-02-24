dddbbeb55bcc0caf78c8ecdd8b5d2839d881fddd
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-02-24 18:21:58 +0100

    Cleanup test to use Testify

--- helpers/compiler_test.go
+++ helpers/compiler_test.go
@@ -12,28 +12,25 @@ func init() {
 	log.SetLevel(log.PanicLevel)
 }
 
-var compilerFiles = []struct {
-	file     string
-	expected string
-}{
-	{"file.cpp", "cxx"},
-	{"test.cxx", "cxx"},
-	{"package/test.cc", "cxx"},
-	{"/outer/inner/file.c++", "cxx"},
-	{"/test.pcc", "cxx"},
-	{"file.c", "cc"},
-	{"/folder/file.c", "cc"},
-}
-
 func TestGetCompilerOptions(t *testing.T) {
+	var compilerFiles = []struct {
+		file     string
+		expected string
+	}{
+		{"file.cpp", "cxx"},
+		{"test.cxx", "cxx"},
+		{"package/test.cc", "cxx"},
+		{"/outer/inner/file.c++", "cxx"},
+		{"/test.pcc", "cxx"},
+		{"file.c", "cc"},
+		{"/folder/file.c", "cc"},
+	}
+
 	for _, v := range compilerFiles {
 		actual, err := GetCompilerOptions(v.file)
-		if err != nil {
-			t.Errorf("GetCompilerOptions(%s): expected no error, actual: %s", v, err)
-		}
-		if actual.CompilerEnv != v.expected {
-			t.Errorf("GetCompilerOptions(%s): expected %s, actual %s", v.file, v.expected, actual.CompilerEnv)
-		}
+
+		assert.Nil(t, err)
+		assert.Equal(t, actual.CompilerEnv, v.expected)
 	}
 }
 
