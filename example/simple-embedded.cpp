1a15a888fe004ce61434c3d662fdcb97052571b8
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-03-24 21:13:32 +0100

    Add embedded options example

---
+++ example/simple-embedded.cpp
@@ -0,0 +1,10 @@
+// andrzej lichnerowicz, unlicensed (~public domain)
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello C++ world\n";
+  return 0;
+}
+
+// belit: cc=g++-7 ccpts="-O2 -std=c++17"
