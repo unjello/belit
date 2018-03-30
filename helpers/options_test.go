94d8f540b7a8f417c9849fd1a460d43a8a0a88eb
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-03-30 18:07:08 +0200

    Add modeline tests

---
+++ helpers/options_test.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractModelineOptions(t *testing.T) {
+	var sources = []struct {
+		source   string
+		expected map[string]string
+	}{
+		{`// belit: cxx=g++-7 cxxopts="-O2 -std=c++17"`, map[string]string{"cxx": "g++-7", "cxxopts": "-O2 -std=c++17"}},
+		{`// belit: cxx="g++-7" cxxopts="-O2 -std=c++17"`, map[string]string{"cxx": "g++", "cxxopts": "-O2 -std=c++17"}},
+		{`/* belit: cc=gcc ccopts=-O0 */`, map[string]string{"cc": "gcc", "ccopts": "-O0"}},
+		{`/* belit: cxx="clang-8.0" cxxopts="-O0 -D_WIN32" */`, map[string]string{"cxx": "clang-8.0", "cxxopts": "-O0 -D_WIN32"}},
+	}
+
+	for _, e := range sources {
+		options, err := ExtractModelineOptions(e.source)
+		assert.Nil(t, err)
+		for k, v := range e.expected {
+			assert.Equal(t, v, options[k])
+		}
+	}
+}
